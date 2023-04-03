d167ebf1596021066ede101a0d00d0801576888a
desuvyas1998 <88439088+desuvyas1998@users.noreply.github.com>
2023-04-04 00:03:29 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -13,7 +13,7 @@ try {
   let address; // variable for storing the address of the deployed contract 
 
   // API creation for connection to the chain
-  const wsProvider = new WsProvider('wss://wss-testnet.5ire.network/');
+  const wsProvider = new WsProvider('5GMosZeWFjiJK4HS9rrgitZq7odYDHuyqBvdzJ7HMaSButSR');
   const api = await ApiPromise.create({ provider: wsProvider });
 
   
@@ -31,7 +31,7 @@ try {
   
   
   // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
+  const PHRASE = 'common velvet trap sunset typical left sugar now just gallery vanish abstract';
   const seedUser = mnemonicToMiniSecret(PHRASE);
   const keyring = new Keyring({ type: "ed25519" });
   const userKeyring = keyring.addFromPair(naclKeypairFromSeed(seedUser));
